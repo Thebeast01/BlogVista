5ea08408ee5cc6a5906c6f0919693901126c8b1d
Saif <mohammadsaif0847@gmail.com>
2024-12-02 12:13:32 +0530

    Added Framer Motion Animation In Signup

--- frontend/src/app/uiComponents/SignupBackground.tsx
+++ frontend/src/app/uiComponents/SignupBackground.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 type Props = {
   children: React.ReactElement | React.ReactElement[];
   backgroundURI: string;
@@ -21,7 +20,7 @@ const SignupBackground: React.FC<Props> = ({ children, backgroundURI }) => {
 
         }} />
 
-      <div className="relative z-20 w-full max-w-lg p-8 lg:-translate-x-[300px] xl:-translate-x-[550px]">
+      <div className="relative z-20 w-full max-w-2xl p-8 lg:-translate-x-[300px] xl:-translate-x-[500px]">
 
         {children}
       </div>
