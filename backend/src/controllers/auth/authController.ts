fe6b61e8b45da2e6c8558dd245bb7233f1a690cb
thebeast01 <mohammadsaif0847@gmail.com>
2025-05-18 03:35:09 +0530

    Fixed Deployment error

--- backend/src/controllers/auth/authController.ts
+++ backend/src/controllers/auth/authController.ts
@@ -87,7 +87,8 @@ export const loginUser = async (req: Request, res: Response) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
+      sameSite: "lax",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000
     })
