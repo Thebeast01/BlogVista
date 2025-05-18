8229ae06cc8e2b2129b9cdb04c541fbd4fdcbaef
thebeast01 <mohammadsaif0847@gmail.com>
2025-05-18 12:42:36 +0530

    Fixed

--- backend/src/controllers/auth/authController.ts
+++ backend/src/controllers/auth/authController.ts
@@ -86,9 +86,13 @@ export const loginUser = async (req: Request, res: Response) => {
     const token = jwt.sign({ id: user.id }, jwtSecret as string)
 
     res.cookie("token", token, {
+      httpOnly: false,
       secure: true,
       sameSite: 'none',
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
       maxAge: 7 * 24 * 60 * 60 * 1000
+
+
     })
     res.status(200).json({
       message: "Login successful",
