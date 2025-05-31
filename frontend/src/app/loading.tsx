1ded80e0c4fedf54f1735bcde41e4ddce97afa05
thebeast01 <mohammadsaif0847@gmail.com>
2025-06-01 01:25:42 +0530

    Fixed error

--- frontend/src/app/loading.tsx
+++ frontend/src/app/loading.tsx
@@ -1,5 +1,4 @@
-'use client';
-export const Loading = () => {
+export default function Loading() {
   return (
     <div className="h-screen flex items-center justify-center bg-background">
       <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-primary"></div>
