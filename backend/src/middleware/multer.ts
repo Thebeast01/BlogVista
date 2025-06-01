36d98b57e69e9be1e5a9f116abf0560d0452f708
thebeast01 <mohammadsaif0847@gmail.com>
2025-06-01 12:38:46 +0530

    Muter Updated

--- backend/src/middleware/multer.ts
+++ backend/src/middleware/multer.ts
@@ -1,11 +1,22 @@
+
 import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
+const uploadDir = path.join(__dirname, 'public', 'temp');
+
+// Ensure the directory exists
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './public/temp')
+    cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname)
+    cb(null, file.originalname);
   }
-})
+});
 
-export const upload = multer({ storage })
+export const upload = multer({ storage });
+
